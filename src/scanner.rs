96e27d4931db2a34fd181c8e556ae566a13bed38
root <marcus.arruda@onlineapp.com.br>
2024-10-30 18:27:21 -0300

    adding tokens

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub struct Scanner {}
 
 impl Scanner {
@@ -11,4 +13,81 @@ impl Scanner {
 }
 
 #[derive(Debug)]
-pub struct Token {}
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: Literal,
+    line: u32,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, literal: Literal, line: u32) -> Self {
+        Self {
+            token_type: (token_type),
+            lexeme: (lexeme),
+            literal: (literal),
+            line: (line),
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug)]
+enum Literal {
+    Number(f64),
+    Text(String),
+    Bool(bool),
+    Nil,
+}
+
+#[derive(Debug)]
+pub enum TokenType {
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    Identifier,
+    String,
+    Number,
+
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
+}
