8ffb60038d35bb5ce8a5fead249b9feefc0a95ec
root <marcus.arruda@onlineapp.com.br>
2024-10-30 07:35:33 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::env;
+use std::fs;
+use std::io;
+use std::process;
+
+fn main() {
+    const TOTAL_ARGS_EXPECTED: usize = 2;
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > TOTAL_ARGS_EXPECTED {
+        println!("Usage rlox [script]!");
+        process::exit(64);
+    } else if args.len() == TOTAL_ARGS_EXPECTED {
+        run_file(&args[1]);
+    } else {
+        rum_prompt();
+    }
+}
+
+fn run_file(s: &str) {
+    let file_content = fs::read_to_string(&s).expect("Could not read the file");
+
+    run(&file_content);
+}
+
+fn run(s: &str) {
+    dbg!(s);
+}
+
+fn rum_prompt() {
+    loop {
+        println!("> ");
+
+        let mut line = String::new();
+
+        io::stdin()
+            .read_line(&mut line)
+            .expect("Could not read the line");
+
+        if line.is_empty() {
+            break;
+        }
+
+        run(&line);
+    }
+}
